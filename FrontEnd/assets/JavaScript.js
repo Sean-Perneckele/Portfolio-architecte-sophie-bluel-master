b1503926e01350878fdaa9331fc28566a2fd5963
Sean Perneckele <sean.perneckele@gmail.com>
2023-07-12 11:11:04 +0200

    mise en place de la récupération des donner.

--- FrontEnd/assets/JavaScript.js
+++ FrontEnd/assets/JavaScript.js
@@ -1,10 +1,56 @@
 
+// récupération des donner
+
+// fonction permettant la laison avec l'API
+
 async function donner() {
 
     const reponse = await fetch ("http://localhost:5678/api/works")
 const tableau = await reponse.json();
 
 console.log(tableau);
-}
 
-donner ();
+return tableau;
+
+}
+donner();
+
+const index = 0
+
+async function travaux() {
+
+    //récupération des donner
+
+    const tableau= await donner();
+
+    for(let slide of tableau){
+  
+    const image = slide.imageUrl;
+    const title = slide.title;
+
+    console.log(image, title);
+
+    // création des ellemnt du DOM et de leur contenu
+
+    const gallery = document.querySelector(".gallery");
+    const contenant= document.createElement("figure"); 
+
+    const img = document.createElement("img");
+    img.src= image;
+    img.alt = title;
+
+    const texte = document.createElement("figcaption");
+    texte.innerText= title;
+
+    // Mise en place des éllément crée dans le DOM 
+
+    contenant.appendChild(img);
+    contenant.appendChild(texte);
+    gallery.appendChild(contenant); 
+}
+ 
+}
+travaux()
+
+// mise en place du filtrage
+
