d8a88f26c9d2495f0a394d563cefd317a4970d10
Sean Perneckele <sean.perneckele@gmail.com>
2023-07-31 15:45:41 +0200

    Première étape de connexion avec mise en place du Token.

--- FrontEnd/assets/JavaScript.js
+++ FrontEnd/assets/JavaScript.js
@@ -1,104 +1,181 @@
 
-// récupération des donnée
+// Récupération des données
 
-// fonction permettant la laison avec l'API
+// Fonction permettant la liaison avec l'API
 
 async function donnée() {
 
-    const reponse = await fetch("http://localhost:5678/api/works")
-    const tableau = await reponse.json();
+  const reponse = await fetch("http://localhost:5678/api/works")
+  const tableau = await reponse.json();
 
-    console.log(tableau);
+  console.log(tableau);
 
-    return tableau;
-
+  return tableau;
 }
-
 
 let tableau
 
-
 async function travaux() {
 
-    //récupération des données
+  //récupération des données
 
-    tableau = await donnée()
+  tableau = await donnée()
 
-    construction(tableau)
+  construction(tableau)
 
 }
 
 // mise en place du filtrage
 
-// activation des bouton et récupéréation de leur "data-catégory"
+// Récupération des données
 
-const buttons = document.querySelectorAll("#portfolio button ");
-console.log(buttons);
+async function récupération() {
+  const reponse = await fetch("http://localhost:5678/api/categories");
+  const categories = await reponse.json();
+  return categories;
+}
+
+// Création des boutons
+
+function Bouton(category) {
+  const boutonContainer = document.querySelector("#portfolio .bouton");
+
+  const button = document.createElement("button");
+  button.setAttribute("data-category", category.id);
+  button.textContent = category.name;
+  boutonContainer.appendChild(button);
+}
+
+// Construction du bouton "Tous"
+
+function boutonTous() {
+  const boutonContainer = document.querySelector("#portfolio .bouton");
+
+  const button = document.createElement("button");
+  button.setAttribute("data-category", "0");
+  button.textContent = "Tous";
+  boutonContainer.insertBefore(button, boutonContainer.firstChild);
+  button.classList.add('button--selected');
+}
+
+// Construction des boutons 
 
 let Id
 
+async function construireBoutons() {
+  const categories = await récupération();
 
-buttons.forEach((button) => {
+  categories.forEach((category) => {
+    Bouton(category);
+  });
+  boutonTous();
+
+  const buttons = document.querySelectorAll("#portfolio button ");
+  console.log(buttons);
+
+  buttons.forEach((button, index) => {
     button.addEventListener("click", () => {
-        Id = button.dataset.category;
-        console.log(Id);
-        filtre(Id);
+      Id = button.dataset.category;
+      console.log(Id);
+      filtre(Id);
+
+      // class bouton actif
+      buttons.forEach((btn, idx) => {
+        if (idx === index) {
+          btn.classList.add('button--selected');
+        } else {
+          btn.classList.remove('button--selected');
+        }
+      });
     })
-})
+  })
+}
+
+construireBoutons();
 
 //  misse en place du filtre 
 
 async function filtre(Id) {
 
-    const data = await donnée();
+  const data = await donnée();
 
-    let filtre
+  let filtre
 
-    if (Id === "0") {
-        filtre = data;
-    } else {
-        filtre = data.filter((slide) => slide.categoryId == Id)
-    }
-
-    console.log(filtre)
-
-    construction(filtre)
-
+  if (Id === "0") {
+    filtre = data;
+  } else {
+    filtre = data.filter((slide) => slide.categoryId == Id)
+  }
+  console.log(filtre)
+  construction(filtre)
 }
 
-// création de la fonction construcion 
+// création de la fonction construction 
 
 function construction(data) {
 
-    const gallery = document.querySelector(".gallery")
-    gallery.innerHTML = " "
+  const gallery = document.querySelector(".gallery")
+  gallery.innerHTML = " "
 
-    for (let slide of data) {
+  for (let slide of data) {
 
-        const image = slide.imageUrl;
-        const title = slide.title;
+    const image = slide.imageUrl;
+    const title = slide.title;
 
-        console.log(image, title);
+    console.log(image, title);
 
-        // création des ellemnt du DOM et de leur contenu
+    // Création des éléments du DOM et de leur contenu
 
-        const gallery = document.querySelector(".gallery");
-        const contenant = document.createElement("figure");
+    const gallery = document.querySelector(".gallery");
+    const contenant = document.createElement("figure");
 
-        const img = document.createElement("img");
-        img.src = image;
-        img.alt = title;
+    const img = document.createElement("img");
+    img.src = image;
+    img.alt = title;
 
-        const texte = document.createElement("figcaption");
-        texte.innerText = title;
+    const texte = document.createElement("figcaption");
+    texte.innerText = title;
 
-        // Mise en place des éllément crée dans le DOM 
+    // Mise en place des éléments crée dans le DOM 
 
-        contenant.appendChild(img);
-        contenant.appendChild(texte);
-        gallery.appendChild(contenant);
-    }
+    contenant.appendChild(img);
+    contenant.appendChild(texte);
+    gallery.appendChild(contenant);
+  }
 }
 
-
 travaux()
+
+// on va géré le connextion
+
+const login = document.querySelector(".login");
+
+
+
+login.addEventListener("click", (event) => {
+
+  localStorage.removeItem("token");
+  
+
+})
+
+
+//  ùùùùùùùùùùùùùùùùùù
+
+const modification = document.querySelectorAll(".token")
+
+if (localStorage.getItem("token")) {
+  modification.forEach((element) => {
+    element.classList.remove("token-none");
+  });
+  login.innerText = "logout"
+
+
+} else {
+  modification.forEach((element) => {
+    element.classList.add("token-none");
+  });
+  login.innerText = "login"
+
+}
+
---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,57 @@
+//  page login récupération des formulaires.
+
+let formulaire = document.querySelector(".login");
+
+formulaire.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Récupération des données
+
+    let email = formulaire.querySelector("#email").value;
+    let password = formulaire.querySelector("#password").value;
+
+    console.log(email, password);
+
+    // validation du mail
+
+
+    let regex = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+");
+
+    if (regex.test(email) && password.trim() !== "  ") {
+        console.log("le mail et mots de passe son bon")
+    } else {
+        console.log("il y a une erreur dans le mail ou mot de passe. ")
+    }
+
+    // envoit des donnés
+    const json = {
+        email: email,
+        password: password
+    };
+
+    //création charge utile 
+    const chargeUtile = JSON.stringify(json)
+
+    //Appelle de la fonction fetch
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile,
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            if (data.token) {
+                // stocke le token dans une variable
+                const token = data.token;
+                // stocker le token dans le local storage
+                localStorage.setItem('token', token);
+                console.log("Authentification réussie !");
+
+                window.location.href = 'index.html';
+            } else {
+                console.log("Authentification échouée. Vérifiez vos informations de connexion.");
+            }
+        });
+})
